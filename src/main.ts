ba99a3277eab5db25a6697a99ac64dad552b9f1a
André Neves <andreneves3@gmail.com>
2019-09-06 23:29:52 -0300

    Ajustes para execução no servidor

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
         AppModule,
         new FastifyAdapter()
     );
-    await app.listen(3000, '0.0.0.0');
+    await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
