cc9e518aa47376a94dd61dce0fcbcff1088784f8
André Neves <andreneves3@gmail.com>
2019-09-15 13:22:22 -0300

    Usuário default (de teste) com role `cliente`

--- src/common/security/user/roles.guard.ts
+++ src/common/security/user/roles.guard.ts
@@ -31,7 +31,7 @@ export class RolesGuard implements CanActivate {
         const user = {
             roles: [
                 'admin',
-                //'cliente'
+                'cliente'
             ]
         };
 
