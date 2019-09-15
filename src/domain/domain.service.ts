4deb6f8b742843b0c5abf2c22217aacdda61c7fc
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-09-15 11:38:40 -0300

    [?] Estética do código

--- src/domain/domain.service.ts
+++ src/domain/domain.service.ts
@@ -5,21 +5,21 @@ export class DomainService {
     getApiRoutes() {
         return [
             {
-                "desc": "GraphQl Feast",
-                "route": "/graphql/feast"
-              },
-              {
-                "desc": "GraphQl Supper",
-                "route": "/graphql/supper"
-              },
-              {
-                "desc": "GraphQl Pedilândia",
-                "route": "/graphql/pedilandia"
-              },
-              {
-                "desc": "GraphQl Genérico",
-                "route": "/graphql/generico"
-              }
+              "desc": "GraphQl Feast",
+              "route": "/graphql/feast"
+            },
+            {
+              "desc": "GraphQl Supper",
+              "route": "/graphql/supper"
+            },
+            {
+              "desc": "GraphQl Pedilândia",
+              "route": "/graphql/pedilandia"
+            },
+            {
+              "desc": "GraphQl Genérico",
+              "route": "/graphql/generico"
+            }
         ]
     }
 }
