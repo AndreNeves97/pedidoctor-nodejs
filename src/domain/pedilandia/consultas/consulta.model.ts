e1678088463dbe7082d913f047fe33649c66cbf9
André Neves <andreneves3@gmail.com>
2019-09-15 13:23:48 -0300

    Remoção de `props` do model ConsultaCreateInput e adição de condição `nullable` para alguns campos

--- src/domain/pedilandia/consultas/consulta.model.ts
+++ src/domain/pedilandia/consultas/consulta.model.ts
@@ -29,52 +29,43 @@ export class Consulta extends Typegoose{
     tipoConsulta: string;
 
     @prop({ required: false })
-    @Field(type => [String])
+    @Field(type => [String], { nullable: true })
     sintomasObservados: string[];
     
     @prop({ required: false })
-    @Field(type => [String])
+    @Field(type => [String], { nullable: true })
     medicamentosQueToma: string[];
 
     @prop({ required: false })
-    @Field(type => [String])
+    @Field(type => [String], { nullable: true })
     doencasRecentes: string[];
 
     @prop({ required: false })
-    @Field(type => [String])
+    @Field(type => [String], { nullable: true })
     informacoesAdicionais: string[];
 
 }
 
 @InputType()
 export class ConsultaCreateInput {
-
-    @prop({ required: true })
     @Field()
     dataConsulta: Date;
 
-    @prop({ required: true, ref: User })
     @Field(type => User)
     paciente: User;
 
-    @prop({ required: true })
     @Field()
     tipoConsulta: string;
 
-    @prop({ required: false })
-    @Field(type => [String])
+    @Field(type => [String], { nullable: true })
     sintomasObservados: string[];
     
-    @prop({ required: false })
-    @Field(type => [String])
+    @Field(type => [String], { nullable: true })
     medicamentosQueToma: string[];
 
-    @prop({ required: false })
-    @Field(type => [String])
+    @Field(type => [String], { nullable: true })
     doencasRecentes: string[];
 
-    @prop({ required: false })
-    @Field(type => [String])
+    @Field(type => [String], { nullable: true })
     informacoesAdicionais: string[];
-
 }
