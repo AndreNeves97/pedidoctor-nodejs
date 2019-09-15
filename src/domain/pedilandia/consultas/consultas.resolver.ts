629cd90869d0f25a494536a5813be95981a31177
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-09-15 11:37:21 -0300

    [?] Criando resolver de consulta

---
+++ src/domain/pedilandia/consultas/consultas.resolver.ts
@@ -0,0 +1,39 @@
+import { Resolver, Query, Args, Mutation, Subscription } from '@nestjs/graphql';
+import { Consulta, ConsultaCreateInput } from './consulta.model';
+import { ConsultasService } from './consultas.service';
+import { UseGuards, SetMetadata } from '@nestjs/common';
+import { Roles, RolesGuard } from '../../../common/security/user/roles.guard';
+import { UserOwnerRule } from '../../../common/security/user/user-owner.rule.guard';
+
+@Resolver(of => Consulta)
+export class ConsultasResolver {
+
+    constructor(
+        private readonly service: ConsultasService
+    ) { }
+
+    @Query(returns => [ Consulta ])
+    @Roles('cliente', 'gerente')
+    async consultas () {
+        return await this.service.findAll();
+    }
+
+    @Query(returns => Consulta)
+    @Roles('cliente', 'gerente')
+    async empresa(@Args('id') id: string) {
+        return await this.service.findById(id);
+    }
+
+    @Mutation(returns => Consulta)
+    @Roles('cliente', 'gerente')
+    async createConsulta(@Args('obj') obj: ConsultaCreateInput ) {
+        return await this.service.create(obj);
+    }
+
+    @Mutation(returns => Consulta, { nullable : true })
+    async deleteConsulta(@Args('id') id: string) {
+        return await this.service.delete(id);
+    }
+
+}
+
