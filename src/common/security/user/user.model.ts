fe2e80fb3363353ee7fa9e350f0aeed0003fc8b0
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-09-15 11:38:31 -0300

    [?] Decorando com respectivos annotations para referências de outras classes

--- src/common/security/user/user.model.ts
+++ src/common/security/user/user.model.ts
@@ -2,8 +2,10 @@ import * as mongoose from 'mongoose';
 
 import { prop, Typegoose } from 'typegoose';
 import { IsString } from 'class-validator';
-import { Field, ID } from 'type-graphql';
+import { ObjectType, InputType, Field, ID } from 'type-graphql';
 
+@ObjectType("User")
+@InputType("UserInput")
 export class User extends Typegoose {
     @Field(type => ID)
     _id: string;
