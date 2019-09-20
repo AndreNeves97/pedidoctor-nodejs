7e002b7265e2200a2b02b818bfc334833ec710ed
habacuqueboy@Protonmail.ch <habacuqueboy@Protonmail.ch>
2019-09-20 14:52:19 -0300

    modelagem dos produtos do estoque

--- src/domain/controle-estoque/controle-estoque.module.ts
+++ src/domain/controle-estoque/controle-estoque.module.ts
@@ -3,8 +3,11 @@ import { EmpresaResolver } from './empresa/empresa.resolver';
 import { ItemEstoqueResolver } from './item-estoque/item-estoque.resolver';
 import { ItemEstoqueService } from './item-estoque/item-estoque.service';
 import { EmpresaService } from './empresa/empresa.service';
+import { ItemEstoqueCatController } from './item-estoque-cat/item-estoque-cat.controller';
+import { ItemEstoqueCatService } from './item-estoque-cat/item-estoque-cat.service';
 
 @Module({
-  providers: [EmpresaResolver, ItemEstoqueResolver, ItemEstoqueService, EmpresaService]
+  providers: [EmpresaResolver, ItemEstoqueResolver, ItemEstoqueService, EmpresaService, ItemEstoqueCatService],
+  controllers: [ItemEstoqueCatController]
 })
 export class ControleEstoqueModule {}
---
+++ src/domain/controle-estoque/item-estoque-cat/item-estoque-cat.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('item-estoque-cat')
+export class ItemEstoqueCatController {}
