513eaa7b3902589f0a1ca8a3c47a7cd4fa399777
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 15:48:03 -0300

    [ ? ] - Implementação de tipos de input e de objeto para Vacina-Aplicação

--- src/domain/pedilandia/vacina-aplicacao/vacina-aplicacao.model.ts
+++ src/domain/pedilandia/vacina-aplicacao/vacina-aplicacao.model.ts
@@ -1,3 +1,4 @@
+import { VacinaUpdate, VacinaInput } from './../vacina/vacina.model';
 import * as mongoose from 'mongoose';
 
 import { prop, Typegoose } from 'typegoose';
@@ -5,9 +6,25 @@ import { IsString, IsArray } from 'class-validator';
 import { ObjectType, InputType, Field, ID } from 'type-graphql';
 import { Vacina } from '../vacina/vacina.model';
 
-
+@ObjectType()
 export class VacinaAplicacao extends Typegoose {
     data : Date;
     vacina : Vacina;
+}
+
+@InputType()
+export class VacinaAplicacaoInput {
+    @Field()
+    data : Date;
+    @Field(type => VacinaInput)
+    vacina : VacinaInput;
+}
+
+@InputType()
+export class VacinaAplicacaoUpdate {
+    @Field()
+    data : Date;
+    @Field(type => VacinaUpdate)
+    vacina : VacinaUpdate;
 }
 
