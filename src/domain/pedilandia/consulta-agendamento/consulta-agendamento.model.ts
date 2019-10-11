a4a62790bb71c364974f6388255ee5942ed98208
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 15:46:23 -0300

    [ ? ] - Implementação de tipos de input e de objeto para Consulta-Agendamento e separação de Agendamento Realização

--- src/domain/pedilandia/consulta-agendamento/consulta-agendamento.model.ts
+++ src/domain/pedilandia/consulta-agendamento/consulta-agendamento.model.ts
@@ -1,3 +1,10 @@
+import { AgendamentoRealizacaoInput, AgendamentoRealizacaoUpdate } from './agendamento-realizacao.model';
+import { AgendamentoRealizacao } from './agendamento-realizacao.model';
+import { DiagnosticoInput, DiagnosticoUpdate } from './../diagnostico/diagnostico.model';
+import { SintomaInput, SintomaUpdate } from './../sintoma/sintoma.model';
+import { ConsultaTipoInput, ConsultaTipoUpdate } from './../consulta-tipo/consulta-tipo.model';
+import { ClinicaInput, ClinicaUpdate } from './../clinica/clinica.model';
+import { UsuarioInput, UsuarioUpdate } from './../usuario/usuario.model';
 import * as mongoose from 'mongoose';
 
 import { prop, Typegoose } from 'typegoose';
@@ -11,33 +18,70 @@ import { ConsultaTipo } from '../consulta-tipo/consulta-tipo.model';
 import { Sintoma } from '../sintoma/sintoma.model';
 import { Clinica } from '../clinica/clinica.model';
 
+@ObjectType()
 export class ConsultaAgendamento extends Typegoose{
+    @Field()
     dataAgendada: Date;
-    
+    @Field(type => Usuario)
     paciente : Usuario;
-    
+    @Field(type => Clinica)
     clinica : Clinica;
+    @Field(type => Usuario)
     medico : Usuario; 
-
+    @Field(type => ConsultaTipo)
     tipo: ConsultaTipo;
-
+    @Field(type => [Sintoma])
     sintomasObservados: Sintoma[];
-    
+    @Field(type => [String])
     informacoesAdicionais: string[];
-
+    @Field(type => AgendamentoRealizacao)
     realizacao : AgendamentoRealizacao;
-    
-
-
     @prop()
     @Field()
     createdAt: Date;
 }
 
-
-export class AgendamentoRealizacao extends Typegoose{
-    horarioInicio: Date;
-    horarioFinalizacao : Date;
-
-    diagnostico: Diagnostico;
+@InputType()
+export class ConsultaAgendamentoInput {
+    @Field()
+    dataAgendada: Date;
+    @Field(type => UsuarioInput)
+    paciente : UsuarioInput;
+    @Field(type => ClinicaInput)
+    clinica : ClinicaInput;
+    @Field(type => UsuarioInput)
+    medico : UsuarioInput; 
+    @Field(type => ConsultaTipoInput)
+    tipo: ConsultaTipoInput;
+    @Field(type => [SintomaInput])
+    sintomasObservados: SintomaInput[];
+    @Field(type => [String])
+    informacoesAdicionais: string[];
+    @Field(type => AgendamentoRealizacaoInput)
+    realizacao : AgendamentoRealizacao;
+    @Field()
+    createdAt: Date;
 }
+
+@InputType()
+export class ConsultaAgendamentoUpdate {
+    @Field()
+    dataAgendada: Date;
+    @Field(type => UsuarioUpdate)
+    paciente : UsuarioUpdate;
+    @Field(type => ClinicaUpdate)
+    clinica : ClinicaUpdate;
+    @Field(type => UsuarioUpdate)
+    medico : UsuarioUpdate; 
+    @Field(type => ConsultaTipoUpdate)
+    tipo: ConsultaTipoUpdate;
+    @Field(type => [SintomaUpdate])
+    sintomasObservados: SintomaUpdate[];
+    @Field(type => [String])
+    informacoesAdicionais: string[];
+    @Field(type => AgendamentoRealizacaoUpdate)
+    realizacao : AgendamentoRealizacaoUpdate;
+    @Field()
+    createdAt: Date;
+}
+    
