a9d61f4f5f4de218cf419044208c843080fa9e15
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 15:47:18 -0300

    [ ? ] - Implementação de tipos de input e de objeto para Doença

--- src/domain/pedilandia/doenca/doenca.model.ts
+++ src/domain/pedilandia/doenca/doenca.model.ts
@@ -1,3 +1,4 @@
+import { SintomaUpdate, SintomaInput } from './../sintoma/sintoma.model';
 import * as mongoose from 'mongoose';
 
 import { prop, Typegoose } from 'typegoose';
@@ -5,11 +6,32 @@ import { IsString, IsArray } from 'class-validator';
 import { ObjectType, InputType, Field, ID } from 'type-graphql';
 import { Sintoma } from '../sintoma/sintoma.model';
 
-
+@ObjectType()
 export class Doenca extends Typegoose {
+    @Field()
     nome: string;
+    @Field()
     descricao: string;
-
+    @Field(type => [Sintoma])
     sintomas: Sintoma[];
 }
 
+@InputType()
+export class DoencaInput extends Typegoose {
+    @Field()
+    nome: string;
+    @Field()
+    descricao: string;
+    @Field(type => [SintomaInput])
+    sintomas: SintomaInput[];
+}
+
+@InputType()
+export class DoencaUpdate extends Typegoose {
+    @Field()
+    nome: string;
+    @Field()
+    descricao: string;
+    @Field(type => [SintomaUpdate])
+    sintomas: SintomaUpdate[];
+}
