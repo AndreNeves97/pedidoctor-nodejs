6bbb7fc321f3a9f2e4af3dd0995d1e8fe6401771
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 15:47:45 -0300

    [ ? ] - Implementação de tipos de input e de objeto para Vacina

--- src/domain/pedilandia/vacina/vacina.model.ts
+++ src/domain/pedilandia/vacina/vacina.model.ts
@@ -1,3 +1,5 @@
+import { VacinaTipoInput, VacinaTipoUpdate } from './../vacina-tipo/vacina-tipo.model';
+import { DoencaInput, DoencaUpdate } from './../doenca/doenca.model';
 import * as mongoose from 'mongoose';
 
 import { prop, Typegoose } from 'typegoose';
@@ -6,11 +8,39 @@ import { ObjectType, InputType, Field, ID } from 'type-graphql';
 import { VacinaTipo } from '../vacina-tipo/vacina-tipo.model';
 import { Doenca } from '../doenca/doenca.model';
 
-
+@ObjectType()
 export class Vacina extends Typegoose {
+    @Field()
     nome : string;
+    @Field()
     descricao : string;
+    @Field(type => VacinaTipo)
     tipo: VacinaTipo;
+    @Field(type => [Doenca])
     doencasEvitadas : Doenca[];
+}
+
+@InputType()
+export class VacinaInput {
+    @Field()
+    nome : string;
+    @Field()
+    descricao : string;
+    @Field(type => VacinaTipoInput)
+    tipo: VacinaTipo;
+    @Field(type => [DoencaInput])
+    doencasEvitadas : DoencaInput[];
+}
+
+@InputType()
+export class VacinaUpdate {
+    @Field()
+    nome : string;
+    @Field()
+    descricao : string;
+    @Field(type => VacinaTipoUpdate)
+    tipo: VacinaTipo;
+    @Field(tyoe => [DoencaUpdate])
+    doencasEvitadas : DoencaUpdate[];
 }
 
