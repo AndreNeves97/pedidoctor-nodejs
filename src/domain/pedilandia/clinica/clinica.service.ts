5002796b79925acfa33bf15c751541865482c167
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 15:49:16 -0300

    [ ? ] - Implementação do Service de Clínica

---
+++ src/domain/pedilandia/clinica/clinica.service.ts
@@ -0,0 +1,62 @@
+import { Clinica, ClinicaCreateInput, ClinicaUpdateInput } from './clinica.model';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from 'nestjs-typegoose';
+import { ModelType } from 'typegoose';
+
+@Injectable()
+export class ClinicaService {
+
+    constructor(
+        @InjectModel(Clinica) private model: ModelType<Clinica>
+    ) { }
+
+    async findById (id: string): Promise<Clinica> {
+        return await this.model
+            .findById(id)
+            .populate('secretarios')
+            .populate('medicos')
+            .populate('enfermeiros')
+            .populate('clientes')
+            .lean();
+    }
+
+    async findAll (): Promise<Clinica> {
+        return this.model
+            .find()
+            .populate('secretarios')
+            .populate('medicos')
+            .populate('enfermeiros')
+            .populate('clientes')
+            .sort({ nome: 'asc' })
+            .lean();
+    }
+
+    async create (obj: ClinicaCreateInput): Promise<Clinica> {
+        const created = await this.model.create({
+            obj
+        });
+
+        return this.findById(created._id);
+    }
+
+    async delete ( id: string ) {
+        return await this.model
+            .findOneAndRemove({ _id : id })
+            .populate('secretarios')
+            .populate('medicos')
+            .populate('enfermeiros')
+            .populate('clientes')
+            .lean();
+    }
+
+    async update ( id: string, obj: ClinicaUpdateInput ) {
+        return await this.model
+            .findOneAndUpdate({ _id : id }, obj, { lean : true })
+            .populate('secretarios')
+            .populate('medicos')
+            .populate('enfermeiros')
+            .populate('clientes');
+    }
+
+}
+
