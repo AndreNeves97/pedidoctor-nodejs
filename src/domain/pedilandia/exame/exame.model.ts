e89f9c7338aaa04b08b6653840dec738ba054c4d
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 15:47:26 -0300

    [ ? ] - Implementação de tipos de input e de objeto para Exame

--- src/domain/pedilandia/exame/exame.model.ts
+++ src/domain/pedilandia/exame/exame.model.ts
@@ -1,3 +1,6 @@
+import { ConsultaAgendamentoInput, ConsultaAgendamentoUpdate } from './../consulta-agendamento/consulta-agendamento.model';
+import { DiagnosticoUpdate, DiagnosticoInput } from './../diagnostico/diagnostico.model';
+import { ExameTipoInput, ExameTipoUpdate } from './../exame-tipo/exame-tipo.model';
 import * as mongoose from 'mongoose';
 
 import { prop, Typegoose } from 'typegoose';
@@ -8,10 +11,33 @@ import { ExameTipo } from '../exame-tipo/exame-tipo.model';
 import { ConsultaAgendamento } from '../consulta-agendamento/consulta-agendamento.model';
 import { Diagnostico } from '../diagnostico/diagnostico.model';
 
-
+@ObjectType()
 export class Exame extends Typegoose {
+    @Field(type => ExameTipo)
     tipo : ExameTipo;
+    @Field(type => ConsultaAgendamento)
     consultaParaApresentacao : ConsultaAgendamento;
+    @Field(type => Diagnostico)
     diagnostico : Diagnostico;
+}
+
+@InputType()
+export class ExameInput {
+    @Field(type => ExameTipoInput)
+    tipo : ExameTipoInput;
+    @Field(type => ConsultaAgendamentoInput)
+    consultaParaApresentacao : ConsultaAgendamentoInput;
+    @Field(type => DiagnosticoInput)
+    diagnostico : DiagnosticoInput;
+}
+
+@InputType()
+export class ExameUpdate {
+    @Field(type => ExameTipoUpdate)
+    tipo : ExameTipoUpdate;
+    @Field(type => ConsultaAgendamentoUpdate)
+    consultaParaApresentacao : ConsultaAgendamentoUpdate;
+    @Field(type => DiagnosticoUpdate)
+    diagnostico : DiagnosticoUpdate;
 }
 
