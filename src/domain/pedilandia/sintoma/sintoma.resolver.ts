b6a00b89a6197e77fa8b72ad6726d88076f5699d
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 20:34:42 -0300

    [ ? ] - Atualização de Roles no Resolver de sintomas

--- src/domain/pedilandia/sintoma/sintoma.resolver.ts
+++ src/domain/pedilandia/sintoma/sintoma.resolver.ts
@@ -1,6 +1,7 @@
 import { Query, Resolver, Args, Mutation } from '@nestjs/graphql';
 import { Sintoma, SintomaInput, SintomaUpdate } from './sintoma.model';
 import { SintomaService } from './sintoma.service';
+import { Roles } from '../../../common/security/user/roles.guard';
 
 @Resolver(of => Sintoma)
 export class SintomaResolver {
@@ -10,26 +11,31 @@ export class SintomaResolver {
     ) { }
 
     @Query(returns => [ Sintoma ])
+    @Roles('user', 'cliente', 'gerente')
     async sintomas () {
         return await this.service.findAll();
     }
 
     @Query(returns => Sintoma)
+    @Roles('user', 'cliente', 'gerente')
     async sintoma ( @Args('id') id: string ) {
         return await this.service.findAll();
     }
 
     @Mutation(returns => Sintoma)
+    @Roles('user', 'cliente', 'gerente')
     async createSintoma ( @Args('obj') obj: SintomaInput ) {
         return await this.service.create(obj);
     }
 
     @Mutation(returns => Sintoma, { nullable : true })
+    @Roles('user', 'cliente', 'gerente')
     async deleteSintoma ( @Args('id') id: string ) {
         return await this.service.delete(id);
     }
 
     @Mutation(returns => Sintoma, { nullable : true })
+    @Roles('user', 'cliente', 'gerente')
     async updateSintoma ( @Args('id') id: string, @Args('obj') obj: SintomaUpdate ) {
         return this.service.update(id, obj);
     }
