dcde942915f3ca56f9b076e627de709244bca54e
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 15:48:25 -0300

    [ ? ] - Implementação de tipos de input e de objeto para Usuário e remoção do uso medicamento

--- src/domain/pedilandia/usuario/usuario.model.ts
+++ src/domain/pedilandia/usuario/usuario.model.ts
@@ -1,3 +1,7 @@
+import { UsoMedicamentoInput, UsoMedicamentoUpdate } from './usomedicamento.model';
+import { UsoMedicamento } from './usomedicamento.model';
+import { AcontecimentoInput, AcontecimentoUpdate } from './../acontecimento/acontecimento.model';
+import { MedicamentoInput, MedicamentoUpdate } from './../medicamento/medicamento.model';
 import * as mongoose from 'mongoose';
 
 import { prop, Typegoose } from 'typegoose';
@@ -10,28 +14,46 @@ import { Exame } from '../exame/exame.model';
 import { Vacina } from '../vacina/vacina.model';
 import { ConsultaAgendamento } from '../consulta-agendamento/consulta-agendamento.model';
 
+@ObjectType()
 export class Usuario extends Typegoose {
     /**
      * Usuário pode ser o pai ou a criança.
      * Se for criança, isPaciente = true
      */
+    @Field()
     isPaciente: boolean;
-
+    
+    @Field(type => [Usuario])
     responsavelPor : Usuario[];
-
+    
+    @Field(type => [UsoMedicamento])
     usoMedicamentos : UsoMedicamento[];
+
+    @Field(type => [Acontecimento])
     acontecimentos : Acontecimento[];
 }
 
-
-
-export class UsoMedicamento extends Typegoose {
-    @prop({ required: true,  ref: Medicamento })
-    medicamento: Medicamento;
-    
-    @prop({ required: true })
-    dataInicio: Date;
-
-    @prop({ required: true })
-    dataFim: Date;
+@InputType()
+export class UsuarioInput {
+    @Field()
+    isPaciente: boolean;
+    @Field(type => [UsuarioInput] )
+    responsavelPor : Usuario[];
+    @Field(type => [UsoMedicamentoInput])
+    usoMedicamentos : UsoMedicamentoInput[];
+    @Field(type => [AcontecimentoInput])
+    acontecimentos : AcontecimentoInput[];
 }
+
+@InputType()
+export class UsuarioUpdate {
+    @Field()
+    isPaciente: boolean;
+    @Field(type => [UsuarioUpdate])
+    responsavelPor : Usuario[];
+    @Field(type => [UsoMedicamentoUpdate])
+    usoMedicamentos : UsoMedicamentoUpdate[];
+    @Field(type => [AcontecimentoUpdate])
+    acontecimentos : AcontecimentoUpdate[];
+}
+
