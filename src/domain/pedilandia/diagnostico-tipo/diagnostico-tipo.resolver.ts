fb8797a037c1c856dd03387fe74d8a0da6d7aaff
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 18:30:20 -0300

    [ ? ] - Implementação do resolver de tipo de diagnóstico

---
+++ src/domain/pedilandia/diagnostico-tipo/diagnostico-tipo.resolver.ts
@@ -0,0 +1,44 @@
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { DiagnosticoTipoService } from './diagnostico-tipo.service';
+import { Query } from '@nestjs/graphql';
+import { DiagnosticoTipo, DiagnosticoTipoInput, DiagnosticoTipoUpdate } from './diagnostico-tipo.model';
+import { Roles, RolesGuard } from '../../../common/security/user/roles.guard';
+
+@Resolver(of => DiagnosticoTipo)
+export class DiagnosticoTipoResolver {
+
+    constructor(
+        private readonly service: DiagnosticoTipoService
+    ) { }
+
+    @Query(returns => [ DiagnosticoTipo ])
+    @Roles('user', 'cliente', 'gerente')   
+    async diagnosticosTipo ( ) {
+        return this.service.findAll();
+    }
+
+    @Query(returns => DiagnosticoTipo)
+    @Roles('user', 'cliente', 'gerente')   
+    async diagnosticoTipo ( @Args('id') id: string )  {
+        return this.service.findById(id);
+    }
+
+    @Mutation(returns => DiagnosticoTipo)
+    @Roles('user', 'cliente', 'gerente')   
+    async createDiagnosticoTipo ( @Args('obj') obj: DiagnosticoTipoInput ) {
+        return this.service.create(obj);
+    }
+
+    @Mutation(returns => DiagnosticoTipo, { nullable : true })
+    @Roles('user', 'cliente', 'gerente')   
+    async deleteDiagnosticoTipo (@Args('id') id: string) {
+        return this.service.delete(id);
+    }
+
+    @Mutation(returns => DiagnosticoTipo, { nullable : true })
+    @Roles('user', 'cliente', 'gerente')   
+    async updateDiagnosticoTipo (@Args('id') id: string, @Args('obj') obj: DiagnosticoTipoUpdate) {
+        return this.service.update(id, obj);
+    }
+
+}
