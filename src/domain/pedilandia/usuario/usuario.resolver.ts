59c5861df3bee157e2e1a51d1cb4a70b7bb2cdc5
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 17:27:55 -0300

    [ ? ] - Implementando Resolver de Usuario

---
+++ src/domain/pedilandia/usuario/usuario.resolver.ts
@@ -0,0 +1,43 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Usuario, UsuarioInput, UsuarioUpdate } from './usuario.model';
+import { UsuarioService } from './usuario.service';
+import { Roles } from '../../../common/security/user/roles.guard';
+
+@Resolver(of => Usuario)
+export class UsuarioResolver {
+
+    constructor (
+        private readonly service: UsuarioService
+    ) { }
+
+    @Query(returns => [ Usuario ])
+    @Roles('user', 'cliente', 'gerente')
+    async usuarios () {
+        return await this.service.findAll();
+    }
+
+    @Query(returns => Usuario)
+    @Roles('user', 'cliente', 'gerente')
+    async usuario ( @Args('id') id: string ) {
+        return await this.service.findById(id);
+    }
+
+    @Mutation(returns => Usuario)
+    @Roles('user', 'cliente', 'gerente')
+    async createUsuario ( @Args('obj') obj: UsuarioInput ) {    
+        return await this.service.create(obj);
+    }
+
+    @Mutation(returns => Usuario, { nullable : true })
+    @Roles('user', 'cliente', 'gerente')
+    async deleteUsuario ( @Args('id') id: string ) {
+        return await this.service.delete(id);
+    }
+
+    @Mutation(returns => Usuario, { nullable : true })
+    @Roles('user', 'cliente', 'gerente')
+    async updateUsuario ( @Args('id') id:string, @Args('obj') obj: UsuarioUpdate ) {
+        return await this.service.update(id, obj);
+    }
+
+}
