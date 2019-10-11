ea9d13f84c73c1ea08d8ac687a652b4785a68392
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 19:51:03 -0300

    [ ? ] - Implementação do Service de Doença

---
+++ src/domain/pedilandia/doenca/doenca.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from 'nestjs-typegoose';
+import { Doenca, DoencaInput, DoencaUpdate } from './doenca.model';
+import { ModelType } from 'typegoose';
+
+@Injectable()
+export class DoencaService {
+
+    constructor (
+        @InjectModel(Doenca) private model: ModelType<Doenca>
+    ) { }
+    
+    async findById ( id: string ): Promise<Doenca> {
+        return await this.model
+            .findById(id)
+            .populate('sintomas')
+            .lean();
+    }
+
+    async findAll ( ): Promise<Doenca> {
+        return await this.model 
+            .find()
+            .populate('sintomas')
+            .sort({ nome : 'asc' })
+            .lean();
+    }
+
+    async create ( obj: DoencaInput ): Promise<Doenca> {
+        const created = await this.model.create({
+            obj
+        });
+
+        return this.findById(created._id);
+    }
+
+    async delete ( id: string ) {
+        return this.model
+            .findOneAndRemove({ _id : id })
+            .populate('sintomas')
+            .lean();
+    }
+
+    async update ( id: string, obj: DoencaUpdate ) {
+        return this.model
+            .findOneAndUpdate({ _id : id }, obj, { lean : true })
+            .populate('sintomas');
+    }
+
+}
