031bcd4eb178f1c0cbb5910bdb2184fac1f74a34
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 20:35:19 -0300

    [ ? ] - Importação do Service e Resolver de Medicamentos no módulo Pedilândia

--- src/domain/pedilandia/pedilandia.module.ts
+++ src/domain/pedilandia/pedilandia.module.ts
@@ -11,6 +11,7 @@ import { ConsultaTipoResolver } from './consulta-tipo/consulta-tipo.resolver';
 import { DiagnosticoTipoResolver } from './diagnostico-tipo/diagnostico-tipo.resolver';
 import { DoencaResolver } from './doenca/doenca.resolver';
 import { ExameTipoResolver } from './exame-tipo/exame-tipo.resolver';
+import { MedicamentoResolver } from './medicamento/medicamento.resolver';
 import { SintomaResolver } from './sintoma/sintoma.resolver';
 import { UsuarioResolver } from './usuario/usuario.resolver';
 
@@ -21,6 +22,7 @@ import { ConsultaTipoService } from './consulta-tipo/consulta-tipo.service';
 import { DiagnosticoTipoService } from './diagnostico-tipo/diagnostico-tipo.service';
 import { DoencaService } from './doenca/doenca.service';
 import { ExameTipoService } from './exame-tipo/exame-tipo.service';
+import { MedicamentoService } from './medicamento/medicamento.service';
 import { SintomaService } from './sintoma/sintoma.service';
 import { UsuarioService } from './usuario/usuario.service';
 
@@ -31,6 +33,7 @@ import { ConsultaTipo } from './consulta-tipo/consulta-tipo.model';
 import { DiagnosticoTipo } from './diagnostico-tipo/diagnostico-tipo.model';
 import { Doenca } from './doenca/doenca.model';
 import { ExameTipo } from './exame-tipo/exame-tipo.model';
+import { Medicamento } from './medicamento/medicamento.model';
 import { Sintoma } from './sintoma/sintoma.model';
 import { Usuario } from './usuario/usuario.model';
 
@@ -101,6 +104,14 @@ import { Usuario } from './usuario/usuario.model';
       }
     }]),
 
+    TypegooseModule.forFeature([{
+      typegooseClass: Medicamento,
+      schemaOptions: {
+        collection: 'Pedilandia_Medicamento',
+        timestamps: true
+      }
+    }]),
+
   ],
   controllers: [],
   exports: [],
@@ -113,6 +124,7 @@ import { Usuario } from './usuario/usuario.model';
     DiagnosticoTipoResolver,
     DoencaResolver,
     ExameTipoResolver,
+    MedicamentoResolver,
     SintomaResolver,
     UsuarioResolver,
 
@@ -123,6 +135,7 @@ import { Usuario } from './usuario/usuario.model';
     DiagnosticoTipoService,
     DoencaService,
     ExameTipoService,
+    MedicamentoService,
     SintomaService,
     UsuarioService,
   
