e8d513e4897ba9f392d306886fab19829aecaa17
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 20:19:53 -0300

    [ ? ] - Implementanção de Service de sintomas

---
+++ src/domain/pedilandia/sintoma/sintoma.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from 'nestjs-typegoose';
+import { Sintoma, SintomaInput, SintomaUpdate } from './sintoma.model';
+import { ModelType } from 'typegoose';
+
+@Injectable()
+export class SintomaService {
+
+    constructor ( 
+        @InjectModel(Sintoma) private model: ModelType<Sintoma>
+    ) { }
+
+    async findById ( id: string ): Promise<Sintoma> {
+        return this.model
+            .findById(id)
+            .lean();
+    }
+
+    async findAll (): Promise<Sintoma> {
+        return this.model
+            .find()
+            .sort({ nome : 'asc' })
+            .lean();
+    }
+
+    async create ( obj: SintomaInput ): Promise<Sintoma> {
+        const created = await this.model.create({
+            obj
+        });
+
+        return this.findById(created._id);
+    }
+
+    async delete ( id: string ) {
+        return this.model
+            .findOneAndRemove({ _id : id })
+            .lean();
+    }
+
+    async update ( id: string, obj: SintomaUpdate ) {
+        return this.model   
+            .findOneAndUpdate({ _id : id }, obj, { lean : true });
+    }
+
+}
