ad39abcc5c1f530ba2a99626e4d6513f08de399d
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 18:30:12 -0300

    [ ? ] - Implementação do service de tipo de diagnóstico

---
+++ src/domain/pedilandia/diagnostico-tipo/diagnostico-tipo.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from 'nestjs-typegoose';
+import { DiagnosticoTipo, DiagnosticoTipoInput, DiagnosticoTipoUpdate } from './diagnostico-tipo.model';
+import { ModelType } from 'typegoose';
+
+@Injectable()
+export class DiagnosticoTipoService {
+
+    constructor ( 
+        @InjectModel(DiagnosticoTipo) private model: ModelType<DiagnosticoTipo>
+    ) { }
+
+    async findById ( id: string ): Promise<DiagnosticoTipo> {
+        return await this.model
+            .findById(id)
+            .lean();
+    }
+
+    async findAll (): Promise<DiagnosticoTipo> {
+        return await this.model
+            .find()
+            .sort({ nome : 'asc' })
+            .lean();
+    }
+
+    async create ( obj: DiagnosticoTipoInput ): Promise<DiagnosticoTipo> {
+        const created = await this.model.create({
+            obj
+        });
+
+        return this.findById(created._id);
+    }
+
+    async delete ( id: string ) {
+        return await this.model
+            .findOneAndRemove({ _id : id })
+            .lean();
+    }
+
+    async update ( id: string, obj: DiagnosticoTipoUpdate ) {
+        return this.model
+            .findOneAndUpdate({ _id : id }, obj, { lean : true });
+    }
+
+}
