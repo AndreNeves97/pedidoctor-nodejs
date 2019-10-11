30941ea39dcf36ff629aa0676db2d7eaed0ce1d7
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 15:48:59 -0300

    [ ? ] - Implementação de tipos de input e de objeto para Medicamento

--- src/domain/pedilandia/medicamento/medicamento.model.ts
+++ src/domain/pedilandia/medicamento/medicamento.model.ts
@@ -1,3 +1,4 @@
+import { DoencaInput, DoencaUpdate } from './../doenca/doenca.model';
 import * as mongoose from 'mongoose';
 
 import { prop, Typegoose } from 'typegoose';
@@ -5,11 +6,32 @@ import { IsString, IsArray } from 'class-validator';
 import { ObjectType, InputType, Field, ID } from 'type-graphql';
 import { Doenca } from '../doenca/doenca.model';
 
-
+@ObjectType()
 export class Medicamento extends Typegoose {
+    @Field()
     nome: string;
+    @Field()
     descricao: string;
-
+    @Field(type => [Doenca])
     indicadoPara: Doenca[];
 }
 
+@InputType()
+export class MedicamentoInput {
+    @Field()
+    nome: string;
+    @Field()
+    descricao: string;
+    @Field(type => [DoencaInput])
+    indicadoPara: Doenca[];
+}
+
+@InputType()
+export class MedicamentoUpdate {
+    @Field()
+    nome: string;
+    @Field()
+    descricao: string;
+    @Field(type => [DoencaUpdate])
+    indicadoPara: Doenca[];
+}
