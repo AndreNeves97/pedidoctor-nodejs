f6b6786d1c70b151c1342f0d479a1fe8490860ce
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 15:48:44 -0300

    [ ? ] - Implementação de tipos de input e de objeto para Sintoma

--- src/domain/pedilandia/sintoma/sintoma.model.ts
+++ src/domain/pedilandia/sintoma/sintoma.model.ts
@@ -4,9 +4,27 @@ import { prop, Typegoose } from 'typegoose';
 import { IsString, IsArray } from 'class-validator';
 import { ObjectType, InputType, Field, ID } from 'type-graphql';
 
-
+@ObjectType()
 export class Sintoma extends Typegoose {
+    @Field()
     nome: string;
+    @Field()
+    descricao: string;
+}
+
+@InputType()
+export class SintomaInput extends Typegoose {
+    @Field()
+    nome: string;
+    @Field()
+    descricao: string;
+}
+
+@InputType()
+export class SintomaUpdate extends Typegoose {
+    @Field()
+    nome: string;
+    @Field()
     descricao: string;
 }
 
