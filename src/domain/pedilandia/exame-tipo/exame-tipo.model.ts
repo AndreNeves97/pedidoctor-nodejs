1bca71612f9c2e04719c8fa8d3e9f344c0c386a9
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-11 15:47:34 -0300

    [ ? ] - Implementação de tipos de input e de objeto para Exame-Tipo

--- src/domain/pedilandia/exame-tipo/exame-tipo.model.ts
+++ src/domain/pedilandia/exame-tipo/exame-tipo.model.ts
@@ -5,9 +5,27 @@ import { IsString, IsArray } from 'class-validator';
 import { ObjectType, InputType, Field, ID } from 'type-graphql';
 import { Sintoma } from '../sintoma/sintoma.model';
 
-
+@ObjectType()
 export class ExameTipo extends Typegoose {
+    @Field()
     nome: string;
+    @Field()
+    descricao: string;
+}
+
+@InputType()
+export class ExameTipoInput {
+    @Field()
+    nome: string;
+    @Field()
+    descricao: string;
+}
+
+@InputType()
+export class ExameTipoUpdate {
+    @Field()
+    nome: string;
+    @Field()
     descricao: string;
 }
 
